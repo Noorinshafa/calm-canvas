21c9e2d02637ed354ae8a74ffb440d19f61fd459
Noor <noorinshafa@gmail.com>
2026-07-28 02:10:43 +0500

    Fetch Printify products

--- api/printify.js
+++ api/printify.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.printify.com/v1/shops.json", {
+    const response = await fetch("https://api.printify.com/v1/shops/23619549/products.json", {
       headers: {
         Authorization: `Bearer ${process.env.PRINTIFY_API_TOKEN}`,
       },
